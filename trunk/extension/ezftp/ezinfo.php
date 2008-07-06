878a119603afbccc5cdb3bc98fbd67e11f8b33ec
dpitard <info@example.com>
2008-07-06 11:07:29 +0000

    - changed milestone
    git-svn-id: http://svn.projects.ez.no/ezftp@14 a7ea6dad-4f92-44a7-9151-6a394f389d8a

--- trunk/extension/ezftp/ezinfo.php
+++ trunk/extension/ezftp/ezinfo.php
@@ -5,7 +5,7 @@ class eZFTPInfo
     function info()
     {
         return array( 'Name' => 'eZFTP extension',
-                      'Version' => '1.0.x',
+                      'Version' => '0.0.x',
                       'Copyright' => 'Copyright (C) 2008 Damien Pitard',
                       'License' => 'GNU General Public License v2.0',
                       'Based on the following third-party software' => array( 'Name' => 'nanoFTPd',
