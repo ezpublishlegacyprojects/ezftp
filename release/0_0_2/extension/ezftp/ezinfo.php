8437b14263a4792c7d127372d3e3f372f888bb01
dpitard <info@example.com>
2008-07-06 11:13:48 +0000

    - released 0.0.2 (unstable)
    git-svn-id: http://svn.projects.ez.no/ezftp@17 a7ea6dad-4f92-44a7-9151-6a394f389d8a

--- release/0_0_2/extension/ezftp/ezinfo.php
+++ release/0_0_2/extension/ezftp/ezinfo.php
@@ -5,7 +5,7 @@ class eZFTPInfo
     function info()
     {
         return array( 'Name' => 'eZFTP extension',
-                      'Version' => '0.0.x',
+                      'Version' => '0.0.2',
                       'Copyright' => 'Copyright (C) 2008 Damien Pitard',
                       'License' => 'GNU General Public License v2.0',
                       'Based on the following third-party software' => array( 'Name' => 'nanoFTPd',
